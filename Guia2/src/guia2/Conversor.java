1dc8041887a3a3ae9fde16f2c4c7432ef352c6a1
Auguspo <augustpolet@gmail.com>
2023-04-18 14:28:03 -0300

    first commit

---
+++ Guia2/src/guia2/Conversor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia2;
+
+public class Conversor {
+    
+    private static final double cambio = 412;
+    
+    public static double convetirAPesos(double dolares){
+        return dolares*cambio;
+    }
+    
+    public double convetirADolares(double pesos){
+        return pesos/cambio;
+    }
+}
