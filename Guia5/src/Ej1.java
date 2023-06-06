1639ccdb2f9c591d687fff6d1bad1a2c1ac6ab8d
Auguspo <augustpolet@gmail.com>
2023-06-06 14:38:10 -0300

    guia5

---
+++ Guia5/src/Ej1.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author NOICE
+ */
+public class Ej1 {
+
+    public static void sumarLista(int[] arreglo) {
+        int suma = 0;
+
+        for (int num : arreglo) {
+            suma += num;
+        }
+        System.out.println("La suma del arreglo es: " + suma);
+
+        double promedio = (double) suma / arreglo.length;
+        System.out.println("El promedio del arreglo es: " + promedio);
+
+    }
+    
+    public static int 
+
+}
